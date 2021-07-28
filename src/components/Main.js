2eb29f24e64ee0c3f38b11184b725746584061f8
JP <jpjones22@gmail.com>
2021-07-28 15:18:30 -0700

    error modal created

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
-import { Button, Card, Image, Container } from 'react-bootstrap'
+import ErrorModal from './ErrorModal'
+import { Button, Card, Image, Container, ThemeProvider } from 'react-bootstrap'
 import Axios from 'axios'
 
 
@@ -12,21 +13,41 @@ class Main extends React.Component {
       lat: "0.750",
       lon: "0.756",
       display_name: "Corneria, Lylat System",
-      staticImage: "https://via.placeholder.com/400x400"
+      staticImage: "https://via.placeholder.com/400x400",
+      modalVis: false,
+      errStatus: 1,
+      errMessage: "All Zeros"
     }
+  }
+  modalHandler = (e) => {
+    if (this.state.modalVis === true) {
+      this.setState({ modalVis: false })
+    }
+    else { this.setState({ modalVis: true }) }
+
+    console.log("Close the modal.  Modal State: " + this.state.modalVis);
   }
 
   getLocationData = async () => {
     let loqKey = process.env.REACT_APP_CITY_KEY;
     let URL = `https://us1.locationiq.com/v1/search.php?key=${loqKey}&q=${this.state.cityInput}&format=json`;
-    const res = await Axios.get(URL);
-    this.setState({
-      lat: res.data[0].lat,
-      lon: res.data[0].lon,
-      display_name: res.data[0].display_name
-    });
-    let imageURL = `https://maps.locationiq.com/v3/staticmap?key=${loqKey}&center=${this.state.lat},${this.state.lon}&zoom=15`
-    this.setState({ staticImage: imageURL });
+    try {
+
+      const res = await Axios.get(URL);
+      this.setState({
+        lat: res.data[0].lat,
+        lon: res.data[0].lon,
+        display_name: res.data[0].display_name
+      });
+      let imageURL = `https://maps.locationiq.com/v3/staticmap?key=${loqKey}&center=${this.state.lat},${this.state.lon}&zoom=15`
+      this.setState({ staticImage: imageURL });
+    }
+    catch (e) {
+      console.log('error!', e.response.status, e.response.data.error);
+      this.setState({ errStatus: e.response.status, errMessage: e.response.data.error, modalVis: true })
+
+
+    }
   }
   cityFormChange = (e) => {
     e.preventDefault();
@@ -41,16 +62,19 @@ class Main extends React.Component {
   render() {
     return (
       <>
-        <h1>City Explorer!</h1>
-        <Form className="m4">
-          <Form.Group>
-            <Form.Control onChange={this.cityFormChange} type="text" custom>
-            </Form.Control>
-            <br />
-            <Button onClick={this.handleSubmit}>Explore!</Button>
-          </Form.Group>
-        </Form>
+        <ErrorModal modalVis={this.state.modalVis} modalHandler={this.modalHandler} errStatus={this.state.errStatus} errMessage={this.state.errMessage} />
 
+        <h1>City Explorer!</h1>
+        <Container className="mb-3">
+          <Form className="m4" onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Control onChange={this.cityFormChange} type="text" custom>
+              </Form.Control>
+              <br />
+              <Button onClick={this.handleSubmit}>Explore!</Button>
+            </Form.Group>
+          </Form>
+        </Container>
         <Container>
           <Card style={{ width: '30rem' }} className="text-center mb-3 bg-primary">
             <Card.Title className="p-3 text-white">{this.state.display_name}</Card.Title>
