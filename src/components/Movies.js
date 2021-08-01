f5b3e443aac3f4abe7fca579c1c33eec4f908c9c
JP <jpjones22@gmail.com>
2021-07-31 21:04:50 -0700

    Added movies.js in prep

---
+++ src/components/Movies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+class Movies extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+
+    return (
+      <>
+
+      </>
+    )
+  }
+}
+
+export default Movies;
